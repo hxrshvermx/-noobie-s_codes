f9f49353a3a8235423ee47eae8b55082cea15f98
Sahil Verma <sahilverma0696@gmail.com>
2025-10-24 21:19:18 +0530

    basics_q1

---
+++ 24th_oct/Basic_coding_tasks/Q1/solution.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int sum_(int a,int b){
+    return a+b;
+}
+
+int main(){
+
+    int result = sum_(5,2);
+    cout<< result<< "\n";
+    return 0;
+}
---
+++ 24th_oct/Basic_coding_tasks/Q1/solution.py
@@ -0,0 +1,5 @@
+def sum_(a: int,b: int) -> int:
+    return a+b
+
+result = sum_("5","2")
+print(result)
