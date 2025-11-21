c807c3ab8ef14908d0a6fd0461c0ab8fab62ff13
Sahil Verma <sahilverma0696@gmail.com>
2025-11-21 22:54:23 +0530

    element_exist

---
+++ 24th_oct/DATA_STRUCTURES/q1/solution.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+bool element_exist(int arr,int x){
+    for (int i = sizeof arr; i = x+1; i++){
+        if (i == x){
+            return true;
+        }
+    }
+    return false;
+}
+
+int main(){
+
+    list[] = [1,2,3,4,5,6];
+    bool result = element_exist(list,4);
+    return 0;
+}
---
+++ 24th_oct/DATA_STRUCTURES/q1/solution.py
@@ -0,0 +1,6 @@
+def element_exist(lst,element):
+    return element in lst
+
+list = [1,2,3,4]
+result = element_exist(list,1)
+print (result)
